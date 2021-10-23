b330d08987f7c326ad3c1f423f54434f139110fe
aaahana <e10991aaahana@dpsrkp.net>
2021-10-23 18:00:27 +0530

    final

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth')
+router.get('/', (req, res) => res.render('dashboard'));
+router.get('/map', (req, res) => res.render('map'));
+router.get('/plastic', (req, res) => res.render('plastic'));
+router.get('/marine', (req, res) => res.render('marine'));
+router.get('/oilspill', (req, res) => res.render('oilspill'));
+router.get('/about', (req, res) => res.render('about'));
+router.get('/posts/create', ensureAuthenticated, (req, res) => res.render('create', { username: req.user.name }));
+module.exports = router;
